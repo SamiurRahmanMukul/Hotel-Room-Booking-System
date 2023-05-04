28aaee45350e546e25e66189bb0b385f688d0c1e
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2023-05-04 16:17:22 +0600

    backed api updated and bug fixed

--- backend/src/middleware/morgan.logger.js
+++ backend/src/middleware/morgan.logger.js
@@ -1,16 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const appRoot = require('app-root-path');
+const FileStreamRotator = require('file-stream-rotator');
 const morgan = require('morgan');
 
 function morganLogger() {
-  const LOGS_FOLDER = `${appRoot}/logs`;
+  const LOGS_FOLDER = `${appRoot}/logs/access`;
 
   if (!fs.existsSync(LOGS_FOLDER)) {
     fs.mkdirSync(LOGS_FOLDER);
   }
 
-  const accessLogStream = fs.createWriteStream(path.join(LOGS_FOLDER, 'app-access.log'), { flags: 'a' });
+  // create a rotating write stream
+  const accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYY-MM-DD',
+    filename: path.join(LOGS_FOLDER, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+  });
+
   return (morgan('combined', { stream: accessLogStream }));
 }
 
