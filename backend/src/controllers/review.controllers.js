8aff8d5ed7adcd3da57344adb21c9aaeb7321395
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2023-08-13 10:58:21 +0600

    user self room review edit API developed

--- backend/src/controllers/review.controllers.js
+++ backend/src/controllers/review.controllers.js
@@ -114,8 +114,8 @@ exports.getRoomReviewsList = async (req, res) => {
     // finding by a review by id
     let myReviews = null;
 
-    if (/^[0-9a-fA-F]{24}$/.test(req.params.id)) {
-      myReviews = await Review.find({ room_id: req.params.id })
+    if (/^[0-9a-fA-F]{24}$/.test(req.params.room_id)) {
+      myReviews = await Review.find({ room_id: req.params.room_id })
         .populate('user_id');
     } else {
       return res.status(400).json(errorResponse(
@@ -135,7 +135,7 @@ exports.getRoomReviewsList = async (req, res) => {
     }
 
     // filtering reviews based on different types query
-    const reviewQuery = new MyQueryHelper(Review.find({ room_id: req.params.id })
+    const reviewQuery = new MyQueryHelper(Review.find({ room_id: req.params.room_id })
       .populate('user_id'), req.query)
       .sort()
       .paginate();
@@ -179,6 +179,83 @@ exports.getRoomReviewsList = async (req, res) => {
         total_page: req?.query?.keyword ? Math.ceil(findReviews.length / req.query.limit) : Math.ceil(myReviews.length / req.query.limit),
         current_page: req?.query?.page ? parseInt(req.query.page, 10) : 1
       }
+    ));
+  } catch (error) {
+    res.status(500).json(errorResponse(
+      2,
+      'SERVER SIDE ERROR',
+      error
+    ));
+  }
+};
+
+// TODO: controller for edit self room review
+exports.editSelfRoomReview = async (req, res) => {
+  try {
+    const { rating, message } = req.body;
+
+    // check `rating` filed exits
+    if (!rating) {
+      return res.status(400).json(errorResponse(
+        1,
+        'FAILED',
+        '`rating` filed is required'
+      ));
+    }
+
+    // check `message` filed exits
+    if (!message) {
+      return res.status(400).json(errorResponse(
+        1,
+        'FAILED',
+        '`message` filed is required'
+      ));
+    }
+
+    // finding by a review by id
+    let myReviews = null;
+
+    if (/^[0-9a-fA-F]{24}$/.test(req.params.review_id)) {
+      myReviews = await Review.findById(req.params.review_id)
+        .populate('user_id');
+    } else {
+      return res.status(400).json(errorResponse(
+        1,
+        'FAILED',
+        'Something went wrong. Probably booking id missing/incorrect'
+      ));
+    }
+
+    // check review available
+    if (!myReviews) {
+      return res.status(404).json(errorResponse(
+        4,
+        'UNKNOWN ACCESS',
+        'Review does not exist'
+      ));
+    }
+
+    if (myReviews?.user_id?.id !== req?.user?.id) {
+      return res.status(406).json(errorResponse(
+        6,
+        'UNABLE TO ACCESS',
+        'Sorry! You can update only self room reviews'
+      ));
+    }
+
+    // update review info & save database
+    const updatedReview = await Review.findByIdAndUpdate(
+      req.params.review_id,
+      { rating, message },
+      { runValidators: true, new: true }
+    );
+
+    // success response with the update review
+    res.status(200).json(successResponse(
+      0,
+      'SUCCESS',
+      'Your room review update successful',
+      updatedReview
     ));
   } catch (error) {
     res.status(500).json(errorResponse(
--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -9,16 +9,15 @@
 
 const router = require('express').Router();
 const { isAuthenticatedUser, isBlocked } = require('../middleware/app.authentication');
-const { roomReviewAdd, getRoomReviewsList } = require('../controllers/review.controllers');
+const { roomReviewAdd, getRoomReviewsList, editSelfRoomReview } = require('../controllers/review.controllers');
 
 // route for add user room review
 router.route('/room-review-add/:id').post(isAuthenticatedUser, isBlocked, roomReviewAdd);
 
 // route for get a room review list
-router.route('/get-room-reviews-list/:id').get(isAuthenticatedUser, isBlocked, getRoomReviewsList);
+router.route('/get-room-reviews-list/:room_id').get(getRoomReviewsList);
 
-// routes for edit and delete an room review
-// router.route('/edit-room-review/:id').put(isAuthenticatedUser, isBlocked, editRoomReview);
-// router.route('/delete-room-review/:id').delete(isAuthenticatedUser, isBlocked, isAdmin, deleteRoomReview);
+// route for edit self room review
+router.route('/edit-room-review/:review_id').put(isAuthenticatedUser, isBlocked, editSelfRoomReview);
 
 module.exports = router;
